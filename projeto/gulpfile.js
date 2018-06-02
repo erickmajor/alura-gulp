8f2bb551123a83a3f95629f5561053bac9857e4e
Erick Major <erick.major@yahoo.com.br>
2018-06-02 14:50:28 -0300

    #001: Modificando as pastas que sofrerão o processo de minificação, preservando as imagens originais (src/img -> dist/img).

--- projeto/gulpfile.js
+++ projeto/gulpfile.js
@@ -4,7 +4,7 @@ var gulp = require('gulp')
     , imagemin = require('gulp-imagemin');
 
 gulp.task('build-img', function() {
-    gulp.src('src/img/**/*')
+    gulp.src('dist/img/**/*')
         .pipe(imagemin())
-        .pipe(gulp.dest('src/img'));
+        .pipe(gulp.dest('dist/img'));
 });
