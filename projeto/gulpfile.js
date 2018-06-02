0dadf1f142d8a6048eafd3ca2483000a9493be32
Erick Major <erick.major@yahoo.com.br>
2018-06-02 15:11:48 -0300

    #007: Adicionando a task clean para fazer a limpeza do diretório 'dist'.

--- projeto/gulpfile.js
+++ projeto/gulpfile.js
@@ -4,12 +4,20 @@ var gulp = require('gulp')
     , imagemin = require('gulp-imagemin')
     , clean = require('gulp-clean');
 
-// nova tarefa
+// copy: copia arquivos e diretórios do diretório src para o diretório dist
 gulp.task('copy', function() {
     gulp.src('src/**/*')
         .pipe(gulp.dest('dist'));
 });
 
+// clean: remove todos os arquivos e diretórios do diretório dist
+gulp.task('clean', function() {
+    gulp.src('dist')
+        .pipe(clean());
+});
+
+// build-img: realiza a minificação dos arquivos que encontram-se no diretório 
+//    dist/img
 gulp.task('build-img', function() {
     gulp.src('dist/img/**/*')
         .pipe(imagemin())
