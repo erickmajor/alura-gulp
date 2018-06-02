77a2e794c2fa1643ca8d5a514979f1fd5b1139d3
Erick Major <erick.major@yahoo.com.br>
2018-06-02 14:53:37 -0300

    #002: Adicionando a task copy, para fazer a cópia automatizada das imagens.

--- projeto/gulpfile.js
+++ projeto/gulpfile.js
@@ -3,6 +3,12 @@
 var gulp = require('gulp')
     , imagemin = require('gulp-imagemin');
 
+// nova tarefa
+gulp.task('copy', function() {
+    gulp.src('src')
+        .pipe(gulp.dest('dist'));
+});
+
 gulp.task('build-img', function() {
     gulp.src('dist/img/**/*')
         .pipe(imagemin())
